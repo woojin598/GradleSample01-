a7d90c18816ef556221805d1c576a01bacae1f2f
tj705 <woojin598@nate.com>
2023-08-14 12:20:54 +0900

    first work

---
+++ src/main/java/kr/co/tj/project/GradleSample01Application.java
@@ -0,0 +1,13 @@
+package kr.co.tj.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSample01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSample01Application.class, args);
+	}
+
+}
---
+++ src/main/java/kr/co/tj/project/MainController.java
@@ -0,0 +1,8 @@
+package kr.co.tj.project;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/member")
+public class MainController {
+
+}
